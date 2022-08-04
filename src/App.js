231ea63574607a456efd0518396b07055509765c
muhammed abuhanifa <muham.abuhanifa@gmail.com>
2022-08-04 16:18:57 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox testing</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happen test!</h2>
     </div>
   );
 }
